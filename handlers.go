468c7da801b744fa002673fd9e72a2b8a715f885
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-06-02 13:44:35 +0200

    add createUser endpoint
    add sql schema and queries

--- handlers.go
+++ handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/Katalcha/rss-scraper/internal/database"
 	"github.com/Katalcha/rss-scraper/internal/helpers"
+	"github.com/google/uuid"
 )
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +19,40 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 
 func errHandler(w http.ResponseWriter, r *http.Request) {
 	helpers.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
+
+func (a *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type requestParameters struct {
+		Name string
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := requestParameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, "could not decode parameters")
+		return
+	}
+	if params.Name == "" {
+		helpers.RespondWithError(w, http.StatusBadRequest, "malformed request body")
+		return
+	}
+
+	user, err := a.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, "could not create user")
+		return
+	}
+
+	helpers.RespondWithJSON(w, http.StatusOK, databaseUserToUser(user))
+}
+
+func (a *apiConfig) getUserByAPIKeyHandler(w http.ResponseWriter, r *http.Request) {
+
 }
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Katalcha/rss-scraper/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
@@ -17,6 +24,17 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	dbURL := os.Getenv("DBURL")
+	if dbURL == "" {
+		log.Fatal("DBURL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+
 	// debug flag parsing e.g: --debug
 	dbg := flag.Bool("debug", false, "Enable debug mode")
 	flag.Parse()
@@ -24,11 +42,18 @@ func main() {
 		fmt.Println("Nothing happens here...")
 	}
 
+	apiConfig := apiConfig{
+		DB: dbQueries,
+	}
+
 	mux := http.NewServeMux()
 
 	// readiness, error
 	mux.HandleFunc("GET /v1/healthz", healthzHandler)
 	mux.HandleFunc("GET /v1/err", errHandler)
+
+	mux.HandleFunc("GET /v1/users", apiConfig.getUserByAPIKeyHandler)
+	mux.HandleFunc("POST /v1/users", apiConfig.createUserHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
