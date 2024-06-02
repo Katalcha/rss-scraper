c973348b370ab72305c1e66cf71ed61354cca3a2
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-06-02 17:47:55 +0200

    minor change

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -6,18 +6,21 @@ import (
 	"strings"
 )
 
-const Err_No_Auth_Header_Included = "no authorization header included"
-const Err_Malformed_Auth_Header = "malformed authorization header"
+const Err_No_Auth_Header_Included string = "no authorization header included"
+const Err_Malformed_Auth_Header string = "malformed authorization header"
+
+const Get_Authorization string = "Authorization"
+const Get_Api_Key string = "ApiKey"
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
+	authHeader := headers.Get(Get_Authorization)
 	if authHeader == "" {
 		return "", errors.New(Err_No_Auth_Header_Included)
 	}
 
 	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+	if len(splitAuth) < 2 || splitAuth[0] != Get_Api_Key {
 		return "", errors.New(Err_Malformed_Auth_Header)
 	}
 
